d558b7dee2bdaa7743c1bd28ca57f5a7b6cf0a1c
Darin <59480801+DeeTrigga@users.noreply.github.com>
2024-09-13 19:08:15 +0000

    Removed .env file for security reasons and modified moa.py for better functionality.

--- moa.py
+++ moa.py
@@ -3,18 +3,9 @@ import asyncio
 import os
 import together
 from together import AsyncTogether, Together
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Access the secret key
-TOGETHER_API_KEY = os.getenv('TOGETHER_API_KEY')
-
-# Initialize the clients
-client = Together(api_key=TOGETHER_API_KEY)
-async_client = AsyncTogether(api_key=TOGETHER_API_KEY)
-
+client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
+async_client = AsyncTogether(api_key=os.environ.get("TOGETHER_API_KEY"))
 user_prompt = "What are 3 fun things to do in SF?"
 reference_models = [
     "Qwen/Qwen2-72B-Instruct",
@@ -23,7 +14,7 @@ reference_models = [
     "databricks/dbrx-instruct",
 ]
 aggregator_model = "mistralai/Mixtral-8x22B-Instruct-v0.1"
-aggregator_system_prompt = """You have been provided with a set of responses from various open-source models to the latest user query. Your task is to synthesize these responses into a single, high-quality response. It is crucial to critically evaluate the information provided in these responses, recognizing that some of it may be biased or incorrect. Your response should not simply replicate the given answers but should offer a refined, accurate, and comprehensive reply to the instruction. Ensure your response is well-structured, coherent, and adheres to the highest standards of accuracy and reliability.
+aggreagator_system_prompt = """You have been provided with a set of responses from various open-source models to the latest user query. Your task is to synthesize these responses into a single, high-quality response. It is crucial to critically evaluate the information provided in these responses, recognizing that some of it may be biased or incorrect. Your response should not simply replicate the given answers but should offer a refined, accurate, and comprehensive reply to the instruction. Ensure your response is well-structured, coherent, and adheres to the highest standards of accuracy and reliability.
 
 Responses from models:"""
 
@@ -48,7 +39,7 @@ async def main():
     finalStream = client.chat.completions.create(
         model=aggregator_model,
         messages=[
-            {"role": "system", "content": aggregator_system_prompt + "\n" + "\n".join([f"{i+1}. {str(element)}" for i, element in enumerate(results)])},
+            {"role": "system", "content": aggreagator_system_prompt + "\n" + "\n".join([f"{i+1}. {str(element)}" for i, element in enumerate(results)])},
             {"role": "user", "content": user_prompt},
         ],
         stream=True,
